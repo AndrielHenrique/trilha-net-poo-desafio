f1e229dd29797d831347a625f805fb19963f2821
Andriel <andriel.h@aluno.ifsp.edu.br>
2025-07-22 13:37:16 -0300

    Criando um Sistema e Abstraindo um Celular com POO em C#, Desafio finalizado.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando Nokia:");
+            Nokia nokia = new Nokia(numero: "40028922", modelo: "Nokia 3210", imei: "111999333", memoria: 12);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Testando iPhone:");
+            Iphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 15 Pro", imei: "444555666", memoria: 256);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+        }
+    }
+}
